6455cdb708f1ffb925ed06a2e7ca076a24ac94b9
Douglas Guiatto <douglas.guiatto@gmail.com>
2022-07-19 15:47:59 -0300

    second commit

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -7,30 +9,36 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
-        // http request
+
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        //String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
         String url = "https://alura-filmes.herokuapp.com/conteudos";
-        URI endpoint = new URI(url);
+        URI endpoint = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endpoint).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        //System.out.println(body);
-       
-        // extrair so os dados titulo, poster, classificacao
+
+        // extrair só os dados que interessam (titulo, poster, classificação)
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
-        System.out.println(listaDeFilmes.size());
-        //System.out.println(listaDeFilmes.get(0));
 
-        // exibir e manipular os dados
+        // exibir e manipular os dados 
+        var geradora = new GeradoraDeFigurinhas();
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
             System.out.println();
         }
-
-
     }
 }
